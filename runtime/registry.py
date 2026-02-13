45c512d142ee97efc63e244be62f75193e9647ff
KRISHNAA R K <114340951+rengakrishnaa@users.noreply.github.com>
2026-02-13 19:27:58 +0530

    Cleaned-up files

--- runtime/registry.py
+++
@@ -1,9 +0,0 @@
-class RuntimeRegistry:
-    def __init__(self):
-        self._runtimes = {}
-
-    def register(self, runtime):
-        self._runtimes[runtime.episode_id] = runtime
-
-    def get(self, episode_id):
-        return self._runtimes.get(episode_id)
